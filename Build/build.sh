e3787a7f11d95cf75e14a109e8ed48868d4c9395
matt <>
2013-03-05 17:26:57 +0000

    Changed phar stub to make executable

--- Build/build.sh
+++ Build/build.sh
@@ -2,4 +2,7 @@
 
 BASEDIR=$(cd $(dirname $0);echo $PWD)
 
-php $BASEDIR/../../empir-1.0.0/empir make $BASEDIR/ContentBuild.phar Src/Bootstrap.php $BASEDIR/../ --exclude="*/.git*|*/.git/*|.DS_Store|Build/*|composer.*|*/test/*|*/yuicompressor/*|*/composer.json|*/Makefile|*/*README*|*phpunit*" --include="*/yuicompressor/build/*"
+php $BASEDIR/../../empir-1.0.0/empir make $BASEDIR/contentbuild.phar Src/Bootstrap.php $BASEDIR/../ --exclude="*/.git*|*/.git/*|.DS_Store|Build/*|composer.*|*/test/*|*/yuicompressor/*|*/composer.json|*/Makefile|*/*README*|*phpunit*" --include="*/yuicompressor/build/*"
+
+cp $BASEDIR/contentbuild.phar /usr/local/bin/contentbuild
+chmod +x /usr/local/bin/contentbuild
