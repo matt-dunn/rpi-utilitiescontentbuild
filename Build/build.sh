e956a73997f2db9251a01f891e55021c0bae185b
matt <>
2013-03-06 10:24:57 +0000

    Set dphar.readonly=0

--- Build/build.sh
+++ Build/build.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(cd $(dirname $0);echo $PWD)
 
-php $BASEDIR/../../empir-1.0.0/empir make $BASEDIR/contentbuild.phar Src/Bootstrap.php $BASEDIR/../ --exclude="*/.git*|*/.git/*|.DS_Store|Build/*|composer.*|*/test/*|*/yuicompressor/*|*/composer.json|*/Makefile|*/*README*|*phpunit*" --include="*/yuicompressor/build/*"
+php -dphar.readonly=0 $BASEDIR/../../empir-1.0.0/empir make $BASEDIR/contentbuild.phar Src/Bootstrap.php $BASEDIR/../ --exclude="*/.git*|*/.git/*|.DS_Store|Build/*|composer.*|*/test/*|*/yuicompressor/*|*/composer.json|*/Makefile|*/*README*|*phpunit*" --include="*/yuicompressor/build/*"
 
 cp $BASEDIR/contentbuild.phar /usr/local/bin/contentbuild.phar
 chmod +x /usr/local/bin/contentbuild.phar
