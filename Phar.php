d51d2b3aecb0025b38f9a0456e479c90faeea77c
matt <>
2013-03-12 13:13:32 +0000

    phpcs

--- Phar.php
+++ Phar.php
@@ -3,7 +3,9 @@
 // TODO: test code to allow updting of phar:
 //try {
 //    $phar = new Phar($_SERVER["PHP_SELF"]);
-//    $phar->addFile("/Users/matt/Sites/projects/rpi/RPI/Utilities/ContentBuild/Src/Constants.php", "Src/Constants.php");
+//    $phar->addFile(
+//      "/Users/matt/Sites/projects/rpi/RPI/Utilities/ContentBuild/Src/Constants.php", "Src/Constants.php"
+//    );
 //} catch (PharException $ex) {
 //    echo $ex->getMessage()."\n";
 //} catch (BadMethodCallException $ex) {
