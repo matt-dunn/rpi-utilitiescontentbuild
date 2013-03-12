8dc57a51f3391fbbfa7a27a8f35df8913d8c2b69
matt <>
2013-03-12 11:02:49 +0000

    Updated phar to allow self update possibilities

---
+++ Phar.php
@@ -0,0 +1,13 @@
+<?php
+
+// TODO: test code to allow updting of phar:
+//try {
+//    $phar = new Phar($_SERVER["PHP_SELF"]);
+//    $phar->addFile("/Users/matt/Sites/projects/rpi/RPI/Utilities/ContentBuild/Src/Constants.php", "Src/Constants.php");
+//} catch (PharException $ex) {
+//    echo $ex->getMessage()."\n";
+//} catch (BadMethodCallException $ex) {
+//    echo $ex->getMessage()." (BadMethodCallException)\n";
+//}
+
+require_once __DIR__."/Src/Bootstrap.php";
