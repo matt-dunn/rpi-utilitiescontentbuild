8b0859cb748e53f06ee165d0d3bc88d5ecb0c2d2
Matt Dunn <matt.dunn@ixxus.com>
2013-05-20 14:10:20 +0100

    Throw exception if scheme resolver is not registered

--- Src/RPI/Utilities/ContentBuild/Lib/UriResolver.php
+++ Src/RPI/Utilities/ContentBuild/Lib/UriResolver.php
@@ -85,13 +85,22 @@ class UriResolver extends Object
     ) {
         $scheme = parse_url($uri, PHP_URL_SCHEME);
         if ($scheme != "") {
+            $schemRegistered = false;
             foreach ($this->getResolvers() as $resolver) {
                 if ($resolver->getScheme() == $scheme) {
+                    $schemRegistered = true;
                     $realpath = $resolver->realpath($project, $uri);
                     if ($realpath !== false) {
                         return $realpath;
                     }
                 }
+            }
+            
+            if (!$schemRegistered) {
+                throw new \Exception(
+                    "Unable to locate '$uri' as scheme '$scheme' cannot be resolved. ".
+                    "Ensure '$scheme' is configured as a uriResolvers"
+                );
             }
         }
         
@@ -104,13 +113,22 @@ class UriResolver extends Object
     ) {
         $scheme = parse_url($uri, PHP_URL_SCHEME);
         if ($scheme != "") {
+            $schemRegistered = false;
             foreach ($this->getResolvers() as $resolver) {
                 if ($resolver->getScheme() == $scheme) {
+                    $schemRegistered = true;
                     $relativePath = $resolver->getRelativePath($project, $uri);
                     if ($relativePath !== false) {
                         return $relativePath;
                     }
                 }
+            }
+            
+            if (!$schemRegistered) {
+                throw new \Exception(
+                    "Unable to locate '$uri' as scheme '$scheme' cannot be resolved. ".
+                    "Ensure '$scheme' is configured as a uriResolvers"
+                );
             }
         }
         
