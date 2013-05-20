bff44379f566fcf27664cdd97c31cee635302f9b
Matt Dunn <matt.dunn@ixxus.com>
2013-05-20 10:42:59 +0100

    Added SCSS debugging

--- Src/RPI/Utilities/ContentBuild/Processors/SCSSPHP/ScssCompiler.php
+++ Src/RPI/Utilities/ContentBuild/Processors/SCSSPHP/ScssCompiler.php
@@ -28,4 +28,31 @@ class ScssCompiler extends \scssc
     {
         $this->importCallback = $callback;
     }
+    
+	protected function compileBlock($block) {
+        if ($this->debug && isset($block->children[0])) {
+            $prop = $block->children[0];
+            
+            $filename = null;
+            $lineNumber = null;
+            
+            if (isset($prop[-2]) && is_object($prop[-2])) {
+                $filename = $prop[-2]->sourceName;
+                $lineNumber = substr_count(substr($prop[-2]->buffer, 0, $prop[-1]), "\n");
+            } else {
+                $filename = $this->parser->sourceName;
+                $lineNumber = substr_count(substr($this->parser->buffer, 0, $prop[-1]), "\n");
+            }
+            
+            $outDebug = $this->makeOutputBlock(null);
+            $outDebug->lines [] = sprintf(
+                "@media -sass-debug-info{filename{font-family:file\:\/\/%s}line{font-family:\\00003%d}}",
+                preg_replace("/([\/:.])/", "\\\\$1", realpath($filename)),
+                $lineNumber
+            );
+            $this->scope->children[] = $outDebug;
+        }
+
+        return parent::compileBlock($block);
+	}
 }
